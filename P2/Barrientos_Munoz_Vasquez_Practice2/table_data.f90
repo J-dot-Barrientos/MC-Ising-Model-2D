af92ab958d71b16b8067c757da6790e8b29e3358
Andrea Muñoz Rubio <anmuru2003@gmail.com>
2026-01-02 21:23:28 +0100

    Update table_data.f90

--- P2/Barrientos_Munoz_Vasquez_Practice2/table_data.f90
+++ P2/Barrientos_Munoz_Vasquez_Practice2/table_data.f90
@@ -4,15 +4,16 @@ module table_mod
 
         contains
         subroutine write_table(table) 
-                integer, intent(out) :: table(:)
-                integer :: n, dmn, z
-                real*8 :: beta
+                REAL*16, intent(out) :: table(:)
+                integer :: n, dmn, z, idx
+                real*16 :: beta
 
-                beta = 1 / temp                
-                dmn = 2
+                beta = 1.0d0 / temp                
+                dmn = 2.0d0
                 z = dmn*2
                 do n = -2*z, 2*z, 2
-                        table(n) = exp(-beta * n)
+                	idx = (n + 2*z) / 2 + 1
+                        table(idx) = exp(-beta * n)
                 end do
         end subroutine write_table
 end module table_mod
